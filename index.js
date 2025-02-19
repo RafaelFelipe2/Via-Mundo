9e2b71ee34461096ca5463873873d8b0801634d4
Rafael Gobetti Felipe <rafaelgobettifelipe@hotmai.com>
2025-02-19 13:36:39 -0300

    Meu primeiro commit

---
+++ index.js
@@ -0,0 +1,70 @@
+import express from "express";
+import autenticar from "./seguranca/autenticar.js";
+import session from "express-session";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const porta = 3000;
+const localhost = "0.0.0.0";
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use("/css", express.static(path.join(__dirname, "css")));
+
+
+app.use(session({
+    secret: "m1Nh4Ch4v3S3cR3t4",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 15 // 15 minutos
+    }
+}));
+
+
+app.get("/login", (requisicao, resposta) => {
+    resposta.sendFile(path.resolve("publico/login.html"));
+});
+
+
+app.post("/login", (requisicao, resposta) => {
+    const { usuario, senha } = requisicao.body;
+
+    if (usuario === "admin" && senha === "admin") {
+        requisicao.session.autenticado = true;
+        resposta.redirect('/Pacotes');
+    } else {
+        resposta.redirect('/login');
+    }
+});
+
+app.get("/Pacotes", autenticar, (requisicao, resposta) => {
+    resposta.sendFile(path.resolve("privado/Pacotes.html"));
+});
+
+app.get("/detalhado", autenticar, (requisicao, resposta) => {
+    resposta.sendFile(path.resolve("privado/detalhado.html"));
+});
+
+app.get("/logout", (requisicao, resposta) => {
+    requisicao.session.destroy();
+    resposta.redirect('/login');
+});
+
+app.use(express.static("publico"));
+
+app.use(autenticar, express.static("privado"));
+
+app.get("/login", (requisicao, resposta) => {
+    resposta.sendFile(path.resolve("publico/login.html"));
+});
+
+app.listen(porta, localhost, () => {
+    console.log(`Servidor rodando em http://${localhost}:${porta}`);
+});
+
